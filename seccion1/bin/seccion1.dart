3fd5d4ed232bf516c673816987bc005c98d7768c
mhuanca1234 <109705117+mhuanca1234@users.noreply.github.com>
2023-10-14 17:35:58 -0500

    Add files via upload
    trabajo del idl1

---
+++ seccion1/bin/seccion1.dart
@@ -0,0 +1,5 @@
+import 'package:seccion1/seccion1.dart' as seccion1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${seccion1.calculate()}!');
+}
---
+++ seccion1/test/seccion1_test.dart
@@ -0,0 +1,8 @@
+import 'package:seccion1/seccion1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
